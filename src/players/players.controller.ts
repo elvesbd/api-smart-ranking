875bade3a81079c586a7f0a2dc992869d414b5ad
Elves Brito <elvesbd@yahoo.com>
2021-09-28 19:53:04 -0300

    chore: created player service

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,10 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CreatePlayerDto } from './dto/create-player.dto';
+import { Player } from './interfaces/player.interface';
+import { PlayersService } from './players.service';
 
 @Controller('players')
 export class PlayersController {
+  constructor(private readonly playersService: PlayersService) {}
+
   @Post()
   async createPlayer(@Body() createPlayerDto: CreatePlayerDto) {
-    return createPlayerDto;
+    return this.playersService.createPlayer(createPlayerDto);
+  }
+
+  @Get()
+  async findAllPlayers(): Promise<Player[]> {
+    return this.playersService.findAllPlayers();
+  }
+
+  @Get('email')
+  findPlayerByEmail(@Param('email') email: string): Promise<Player> {
+    return this.findPlayerByEmail(email);
   }
 }
